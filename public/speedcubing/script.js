a84a094b75279152f3cc3fcadd88687bb074ef1b
Auck02 <levibarker2002@gmail.com>
2026-08-30 23:06:01 +0100

    added timestamps

--- public/speedcubing/script.js
+++ public/speedcubing/script.js
@@ -9,12 +9,15 @@ fetch("https://api.levibarker.dev/solves")
 
             const idCell = document.createElement("td");
             const timeCell = document.createElement("td");
+            const timestampCell = document.createElement("td");
 
             idCell.textContent = solve.id;
             timeCell.textContent = solve.time;
+            timestampCell.textContent = solve.timestamp;
 
             row.appendChild(idCell);
             row.appendChild(timeCell);
+            row.appendChild(timestampCell);
 
             tableBody.appendChild(row);
         });
